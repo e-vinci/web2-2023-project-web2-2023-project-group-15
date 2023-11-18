1b3b3805611c8735ed32a33ba70b1e88d0d6c4a6
edufalcao14 <eduardo.sampaio@student.vinci.be>
2023-11-18 20:36:12 +0100

    FrontEnd : Header - Navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import { Navbar as BootstrapNavbar } from 'bootstrap';
 import '../../stylesheets/_navbar.scss';
+import profileIcon from '../../img/navbar/profileIcon.svg';
+import shoppingCart from '../../img/navbar/shoppingCart.svg';
+import GrandiosoVinciText from '../../img/navbar/GrandiosoVinci.svg';
+import logo from '../../img/navbar/logo.svg';
+
 
 /**
  * Render the Navbar which is styled by using Bootstrap
@@ -8,18 +14,36 @@ import '../../stylesheets/_navbar.scss';
  * - the URI associated to a page shall be given in the attribute "data-uri" of the Navbar
  * - the router will show the Page associated to this URI when the user click on a nav-link
  */
-
 const Navbar = () => {
   const navbarWrapper = document.querySelector('#navbarWrapper');
   const navbar = `
-  <div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#about">About</a>
-  <a href="#contact">Contact</a>
-  <input type="text" placeholder="Search..">
-</div>
+  <div class="container-nav">
+    
+    <div class="topnav">
+      <div class="containerLogos">
+        <div class="logoDiv">
+          <a href="#home  class="logo" "><img src="${logo}" alt="profile icon"></a>
+        </div>
+        <img src="${GrandiosoVinciText}" id="shoppingCart" alt="profile icon">
+      </div>
+
+      <div class="containerIcons">
+        <input type="text" placeholder="Search..">
+        <a href="#home"><img src="${profileIcon}" id="profileIcon" alt="profile icon"></a>
+        <a href="#about"><img src="${shoppingCart}" id="shoppingCart" alt="profile icon"></a>
+        <a href="#about" class="navMenu">
+          <div class="menu" ">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </div>
+        </a>
+      </div>
+  </div>
   `;
   navbarWrapper.innerHTML = navbar;
+
 };
+
 
 export default Navbar;
