c659b7aa130d0096c35a8b6b4741446c17f7f4da
Tom Simonis <tom.simonis@student.vinci.be>
2023-11-24 15:49:15 +0100

    Front maj not finished

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,11 +1,15 @@
+/* eslint-disable spaced-comment */
 import imgCaroussel1 from '../../img/home/carroulsel1.jpg';
 import imgBags from '../../img/home/home_categories_bags.png';
 import imgClothing from '../../img/home/home_categories_clothing.png';
 import imgCosmetics from '../../img/home/home_categories_cosmetics.png';
 import imgWatches from '../../img/home/home_categories_watches.png';
-import popularImgWatches from '../../img/home/home_popular_category_watches.png';
-import popularImgBags from '../../img/home/home_popular_category_bags.png';
-import popularImgCosmetics from '../../img/home/home_popular_category_cosmetics.png';
+//import popularImgWatches from '../../img/home/home_popular_category_watches.png';
+//import popularImgBags from '../../img/home/home_popular_category_bags.png';
+//import popularImgCosmetics from '../../img/home/home_popular_category_cosmetics.png';
+import popularImgWatchesBallonBleu from '../../img/products/2.png';
+import popularImgWatchesRolexSubmarinier from '../../img/products/1.png';
+import popularImgWatchesPPGrandComplications from '../../img/products/7.png';
 
 import '../../stylesheets/_home.scss';
 // Importe le fichier Bootstrap CSS
@@ -48,7 +52,7 @@ const html = `
 
  <!-- Categories section -->
 
-    <div class="container-categorie-home"> 
+  <div class="container-categorie-home"> 
       <h1> Categories </h1>
       <div class="flex-container-categories">
         <div class="card text-center mx-auto" >
@@ -79,118 +83,101 @@ const html = `
           <a href="#" class="btn btn-dark">Check</a>
         </div>
       </div>
-    </div>
+    </d iv>
   </div>
+
   <!-- Popular section -->
 
   <div class="flex-container-popular">
 
-    <div class="popular-row">
-     
-      <div class="card text-center" style="width: 20rem;">
-        <img src="${popularImgWatches}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h4 class="card-title">Watches</h4>
-        </div>
-      </div>
+      <div class="container-products-popular" id="container-products-popular">
+              <a class="link-products" href="#" data-rui="" id="card-popular-products">
+                <div class="product-card-img" id="category-popular-img">
+                <h1> watches </h1>
+                </div>
+             </a>
+             <a class="link-products-popular" href="#" data-rui="" >
+                <div class="product-card-popular" id="product-card">
+                    <img class="product-img-popular" src=${popularImgWatchesBallonBleu} alt="Picture Watch Cartier"/>
+                    <h1 class="title-product" >Ballon Bleu de Cartier</h1>
+                    <h2 class="price-product" >9.050.99 €</h1>
+                </div>
+             </a>
+             <a class="link-products-popular" href="#" data-rui="" >
+                <div class="product-card-popular" id="product-card">
+                    <img class="product-img-popular" src=${popularImgWatchesRolexSubmarinier} alt="Picture Watch Rolex"/>
+                    <h1 class="title-product" >Rolex Submariner</h1>
+                    <h2 class="price-product" >25.190.00 €</h1>
+                </div>
+             </a>
+             <a class="link-products-popular" href="#" data-rui="">
+                <div class="product-card-popular" id="product-card">
+                    <img class="product-img-popular" src=${popularImgWatchesPPGrandComplications} alt="Picture Watch Patek Philippe"/>
+                    <h1 class="title-product" >Patek Philippe Grand Complications </h1>
+                    <h2 class="price-product" >89.650.99 €</h1>
+                </div>
+             </a>
+       </div>
 
-      <div class="card" style="width: 18rem;">
-         <img src="..." class="card-img-top" alt="...">
-         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
+      <div class="container-products-popular" id="container-products-popular">
+        <a class="link-products" href="#" data-rui="" id="card-popular-products">
+          <div class="product-card-img" id="category-popular-img">
+          <h1> watches </h1>
+          </div>
+        </a>
+        <a class="link-products-popular" href="#" data-rui="" >
+          <div class="product-card-popular" id="product-card">
+              <img class="product-img-popular" src=${popularImgWatchesBallonBleu} alt="Picture Watch Cartier"/>
+              <h1 class="title-product" >Ballon Bleu de Cartier</h1>
+              <h2 class="price-product" >9.050.99 €</h1>
+          </div>
+        </a>
+        <a class="link-products-popular" href="#" data-rui="" >
+          <div class="product-card-popular" id="product-card">
+              <img class="product-img-popular" src=${popularImgWatchesRolexSubmarinier} alt="Picture Watch Rolex"/>
+              <h1 class="title-product" >Rolex Submariner</h1>
+              <h2 class="price-product" >25.190.00 €</h1>
+          </div>
+        </a>
+        <a class="link-products-popular" href="#" data-rui="">
+          <div class="product-card-popular" id="product-card">
+              <img class="product-img-popular" src=${popularImgWatchesPPGrandComplications} alt="Picture Watch Patek Philippe"/>
+              <h1 class="title-product" >Patek Philippe Grand Complications </h1>
+              <h2 class="price-product" >89.650.99 €</h1>
+          </div>
+        </a>
      </div>
 
-    </div>
-
-    <div class="popular-row">
-
-      <div class="card text-center" style="width: 20rem;">
-        <img src="${popularImgBags}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h4 class="card-title">Bags</h4>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-         <img src="..." class="card-img-top" alt="...">
-         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-     </div>
-
-   </div>
-
-    <div class="popular-row">
-
-      <div class="card text-center" style="width: 20rem;">
-        <img src="${popularImgCosmetics}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h4 class="card-title">Cosmetics</h4>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-         <img src="..." class="card-img-top" alt="...">
-         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-     </div>
-
-    </div>  
-
+     <div class="container-products-popular" id="container-products-popular">
+     <a class="link-products" href="#" data-rui="" id="card-popular-products">
+       <div class="product-card-img" id="category-popular-img">
+       <h1> watches </h1>
+       </div>
+     </a>
+     <a class="link-products-popular" href="#" data-rui="" >
+       <div class="product-card-popular" id="product-card">
+           <img class="product-img-popular" src=${popularImgWatchesBallonBleu} alt="Picture Watch Cartier"/>
+           <h1 class="title-product" >Ballon Bleu de Cartier</h1>
+           <h2 class="price-product" >9.050.99 €</h1>
+       </div>
+     </a>
+     <a class="link-products-popular" href="#" data-rui="" >
+       <div class="product-card-popular" id="product-card">
+           <img class="product-img-popular" src=${popularImgWatchesRolexSubmarinier} alt="Picture Watch Rolex"/>
+           <h1 class="title-product" >Rolex Submariner</h1>
+           <h2 class="price-product" >25.190.00 €</h1>
+       </div>
+     </a>
+     <a class="link-products-popular" href="#" data-rui="">
+       <div class="product-card-popular" id="product-card">
+           <img class="product-img-popular" src=${popularImgWatchesPPGrandComplications} alt="Picture Watch Patek Philippe"/>
+           <h1 class="title-product" >Patek Philippe Grand Complications </h1>
+           <h2 class="price-product" >89.650.99 €</h1>
+       </div>
+     </a>
   </div>
+
+</div>
 
 </div>   `;
 const HomePage = () => {
