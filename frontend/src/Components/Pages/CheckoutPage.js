068a6e0d15a38eb112afd10983359d9b1370b4d9
maureen-renaux-vinci <maureen.renaux@student.vinci.be>
2023-11-26 23:58:32 +0100

    add paypal

--- frontend/src/Components/Pages/CheckoutPage.js
+++ frontend/src/Components/Pages/CheckoutPage.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
+import { loadScript } from "@paypal/paypal-js";
 import { getAuthenticatedUser } from "../../utils/auths";
 // import { getCartTotal } from "../../utils/shoppingCart";
 import Navigate from "../Router/Navigate";
 import OrderLibrary from "../../Domain/OrderLibrary";
 
-const CheckoutPage = async () => {
+const CheckoutPage = () => {
     const user = getAuthenticatedUser();
 
     if(user === undefined){
@@ -106,52 +107,62 @@ const CheckoutPage = async () => {
 
                         <div class="d-block my-3">
                           <div class="custom-control custom-radio">
-                            <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
+                            <input id="credit" name="paymentMethod" type="radio" class="custom-control-input"  checked required>
                             <label class="custom-control-label" for="credit">Credit card</label>
                           </div>
                           <div class="custom-control custom-radio">
-                            <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required>
+                            <input id="debit" name="paymentMethod" type="radio" class="custom-control-input"  required>
                             <label class="custom-control-label" for="debit">Debit card</label>
                           </div>
                           <div class="custom-control custom-radio">
-                            <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
+                            <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input"   required>
                             <label class="custom-control-label" for="paypal">Paypal</label>
                           </div>
                         </div>
-                        <div class="row">
-                          <div class="col-md-6 mb-3">
-                            <label for="cc-name">Name on card</label>
-                            <input type="text" class="form-control" id="cc-name" placeholder="" required>
-                            <small class="text-muted">Full name as displayed on card</small>
-                            <div class="invalid-feedback">
-                              Name on card is required
+
+                        <div id="content1" class="payment-content">
+                          
+                          <div class="row">
+                            <div class="col-md-6 mb-3">
+                              <label for="cc-name">Name on card</label>
+                              <input type="text" class="form-control" id="cc-name" placeholder="" required>
+                              <small class="text-muted">Full name as displayed on card</small>
+                              <div class="invalid-feedback">
+                                Name on card is required
+                              </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                              <label for="cc-number">Credit card number</label>
+                              <input type="text" class="form-control" id="cc-number" placeholder="" required>
+                              <div class="invalid-feedback">
+                                Credit card number is required
+                              </div>
                             </div>
                           </div>
-                          <div class="col-md-6 mb-3">
-                            <label for="cc-number">Credit card number</label>
-                            <input type="text" class="form-control" id="cc-number" placeholder="" required>
-                            <div class="invalid-feedback">
-                              Credit card number is required
+                          <div class="row">
+                            <div class="col-md-3 mb-3">
+                              <label for="cc-expiration">Expiration</label>
+                              <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+                              <div class="invalid-feedback">
+                                Expiration date required
+                              </div>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                              <label for="cc-expiration">CVV</label>
+                              <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+                              <div class="invalid-feedback">
+                                Security code required
+                              </div>
                             </div>
                           </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-md-3 mb-3">
-                            <label for="cc-expiration">Expiration</label>
-                            <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-                            <div class="invalid-feedback">
-                              Expiration date required
-                            </div>
-                          </div>
-                          <div class="col-md-3 mb-3">
-                            <label for="cc-expiration">CVV</label>
-                            <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                            <div class="invalid-feedback">
-                              Security code required
-                            </div>
-                          </div>
-                        </div>
-                        <hr class="mb-4">
+                          <hr class="mb-4">
+                      </div>
+                  
+                      <div id="content2" class="payment-content">
+                        <div id="paypal-button" style="width: 200px"></div>
+                      </div>
+
+
 
                         <button type="button" id="btnCheckout" class="btn btn-primary">Checkout</button>
 
@@ -185,13 +196,41 @@ const CheckoutPage = async () => {
     const main = document.querySelector('main');
     main.innerHTML = html;
 
-    
-      // eslint-disable-next-line no-undef
-      const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+    const script = document.createElement('script');
+    // ajoute la source au script créé
+    script.src = 'https://www.paypal.com/sdk/js?client-id=sd&currency=EUR&buyer-country=DE&commit=false';
+    // Append au main (sinon il n'est pas executé)
+    main.appendChild(script);
+
+    const btnCredit = document.getElementById('credit');
+    const btnDebit = document.getElementById('debit');
+    const btnPaypal = document.getElementById('paypal');
+    const content1 = document.getElementById('content1');
+    const content2 = document.getElementById('content2');
+
+    btnCredit.addEventListener('click' ,async (e) => {
+      e.preventDefault();
+      displayMethodPayment(content1);
+    });
+
+    btnDebit.addEventListener('click' ,async (e) => {
+      e.preventDefault();
+      displayMethodPayment(content1);
+    });
+
+    btnPaypal.addEventListener('click' ,async (e) => {
+      e.preventDefault();
+      paypal();
+      displayMethodPayment(content2);
+    });
+
+
+    // eslint-disable-next-line no-undef
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'));
      
-      const btnCheckout = document.getElementById('btnCheckout');
+    const btnCheckout = document.getElementById('btnCheckout');
       
-      btnCheckout.addEventListener('click', async (e) => {
+    btnCheckout.addEventListener('click', async (e) => {
         e.preventDefault();
         await OrderLibrary.prototype.createOrder();
         myModal.show();
@@ -202,9 +241,44 @@ const CheckoutPage = async () => {
           Navigate('/');
         }, 3000);
         
-      });
+    });
       
 }
+
+function displayMethodPayment(contentElement) {
+  const allContentElements = document.querySelectorAll('.payment-content');
+  allContentElements.forEach(element => {
+    element.style.display = 'none';
+  });
+  contentElement.style.display = 'block';
+}
+
+function paypal() {
+   loadScript({
+      'client-id':
+        'AcxVyDrUVK76TgOexqXYBhJQLc8AC7LIIsZYeI2xJim7Z2vFXGWRkN-4JzN4YeUXo3UjO14mx99K0Hvf',
+    })
+      .then((paypal) => {
+        
+  paypal.Buttons({
+    createOrder: function (data, actions) {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '1.00'
+          }
+        }]
+      });
+    },
+    onApprove: function (data, actions) {
+      return actions.order.capture().then(function (details) {
+        alert('Transaction completed by ' + details.payer.name.given_name);
+      });
+    }
+  }).render('#paypal-button-container');
+})
+}
+
 
 
 
