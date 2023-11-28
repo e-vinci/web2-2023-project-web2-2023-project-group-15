5db2ad3490b44de8a61b7217c184fbf6feab4496
maureenrnx <maureen.renaux@gmail.com>
2023-11-28 15:40:10 +0100

    change style and add new features in adminpage

--- frontend/src/Components/Pages/AdminPage.js
+++ frontend/src/Components/Pages/AdminPage.js
@@ -2,148 +2,166 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {Chart} from 'chart.js/auto';
 import OrderLibrary from '../../Domain/OrderLibrary';
-
-const html = `
-
-<div class="container-fluid">
-<!--  Row 1 -->
-<div class="row">
-  <div class="col-lg-8 d-flex align-items-strech">
-    <div class="card border-white w-100">
-      <div class="card-body">
-        <div class="mb-3 mb-sm-0">
-        <h5 class="card-title fw-semibold">Sales Overview</h5>
-        </div>
-        <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
-          <div>
-          <canvas id="myChart" height="400" width="800"></canvas>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-lg-4">
-    <div class="row">
-      <div class="col-lg-12">
-        <!-- Yearly Breakup -->
-        <div class="card border-white overflow-hidden">
-          <div class="card-body p-4">
-            <h5 class="card-title mb-9 fw-semibold">Yearly Breakup</h5>
-            <div class="row align-items-center">
-              <div class="col-8">
-                <h4 class="fw-semibold mb-3">$36,358</h4>
-                <div class="d-flex align-items-center mb-3">
-                  <span
-                    class="me-1 rounded-circle bg-light-success round-20 d-flex align-items-center justify-content-center">
-                    <i class="ti ti-arrow-up-left text-success"></i>
-                  </span>
-                  <p class="text-dark me-1 fs-3 mb-0">+9%</p>
-                  <p class="fs-3 mb-0">last year</p>
-                </div>
-                <div class="d-flex align-items-center">
-                  <div class="me-4">
-                    <span class="round-8 bg-primary rounded-circle me-2 d-inline-block"></span>
-                    <span class="fs-2">2023</span>
-                  </div>
-                  <div>
-                    <span class="round-8 bg-light-primary rounded-circle me-2 d-inline-block"></span>
-                    <span class="fs-2">2023</span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-4">
-                <div class="d-flex justify-content-center">
-                  <div id="breakup"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-12">
-        <!-- Monthly Earnings -->
-        <div class="card border-white">
-          <div class="card-body">
-            <div class="row alig n-items-start">
-              <div class="col-8">
-                <h5 class="card-title mb-9 fw-semibold"> Monthly Earnings </h5>
-                <h4 class="fw-semibold mb-3">$6,820</h4>
-                <div class="d-flex align-items-center pb-1">
-                  <span
-                    class="me-2 rounded-circle bg-light-danger round-20 d-flex align-items-center justify-content-center">
-                    <i class="ti ti-arrow-down-right text-danger"></i>
-                  </span>
-                  <p class="text-dark me-1 fs-3 mb-0">+9%</p>
-                  <p class="fs-3 mb-0">last year</p>
-                </div>
-              </div>
-              <div class="col-4">
-                <div class="d-flex justify-content-end">
-                  <div
-                    class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
-                    <i class="ti ti-currency-dollar fs-6"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div id="earning"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-  <div class="col-lg-8 d-flex align-items-stretch">
-    <div class="card border-white w-100">
-      <div class="card-body p-4" >
-        <h5 class="card-title fw-semibold mb-4">Recent Transactions</h5>
-        <div class="table-responsive">
-    <table class="table text-nowrap mb-0 align-middle">
-      <thead class="text-dark fs-4">
-        <tr>
-          <th class="border-bottom-0">
-            <h6 class="fw-semibold mb-0">Id</h6>
-          </th>
-          <th class="border-bottom-0">
-            <h6 class="fw-semibold mb-0">Assigned</h6>
-          </th>
-          <th class="border-bottom-0">
-            <h6 class="fw-semibold mb-0">Name</h6>
-          </th>
-          <th class="border-bottom-0">
-            <h6 class="fw-semibold mb-0">Priority</h6>
-          </th>
-          <th class="border-bottom-0">
-            <h6 class="fw-semibold mb-0">Budget</h6>
-          </th>
-        </tr>
-      </thead>
-      <tbody id="recentTransactions">
-      
-      </tbody>
-      </table>
-      </div>
-      </div>
-    </div>
-  </div>
-</div>`;
+import '../../stylesheets/_admin.scss';
 
 const AdminPage = async () => {
-    const main = document.querySelector('main');
-    main.innerHTML = html;
-    const orders = await OrderLibrary.prototype.getAllOrder();
-    console.log(orders);
-    const recentTransactions = document.getElementById('recentTransactions');
+let html = ``;
+const main = document.querySelector('main');
+main.innerHTML = html;
+const orders = await OrderLibrary.prototype.getAllOrder(); 
+const totalPrice = await getTotalPriceOrder(orders);
+
+html += `
+<div id="content-wrapper" class="d-flex flex-column">
+    <div class="container-fluid">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        </div>
+
+        <!-- Content Row -->
+        <div class="row">
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Earnings (Monthly)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Earnings (Annual) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Earnings (Annual)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">${totalPrice}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Tasks Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                </div>
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <div class="h5 mb-0 mr-4 font-weight-bold text-gray-800"> 50%</div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="progress progress-sm mr-2">
+                                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%"
+                                                aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+       
+            <div class="row ">
+              <div class="col-xl-8 col-lg-7">
+                <div class="card shadow mb-10">
+                        <div class="col-lg-4 d-flex align-items-stretch">
+                            <div class="card border-white w-110">
+                                <div class="card-body p-4">
+                                    <div class="mb-7 mb-sm-2">
+                                        <h5 class="card-title fw-semibold">Sales Overview</h5>
+                                    </div>
+                                    <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
+                                        <div><canvas id="myChart" height="400" width="900"></canvas></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-8">
+                  <div class="card shadow mb-9">
+                      <div class="col-lg d-flex align-items-stretch">
+                          <div class="card border-white w-100">
+                              <div class="card-body p-4">
+                                  <h5 class="card-title fw-semibold mb-4">Recent Transactions</h5>
+                                  <div class="table-responsive">
+                                      <table class="table text-nowrap mb-0 align-middle">
+                                          <thead class="text-dark fs-4">
+                                              <tr>
+                                                  <th class="border-bottom-0">
+                                                      <h6 class="fw-semibold mb-0">Id</h6>
+                                                  </th>
+                                                  <th class="border-bottom-0">
+                                                      <h6 class="fw-semibold mb-0">Assigned</h6>
+                                                  </th>
+                                                  <th class="border-bottom-0">
+                                                      <h6 class="fw-semibold mb-0">Name</h6>
+                                                  </th>
+                                                  <th class="border-bottom-0">
+                                                      <h6 class="fw-semibold mb-0">Priority</h6>
+                                                  </th>
+                                                  <th class="border-bottom-0">
+                                                      <h6 class="fw-semibold mb-0">Budget</h6>
+                                                  </th>
+                                              </tr>
+                                          </thead>
+                                          <tbody id="recentTransactions">
+
+                                          </tbody>
+                                      </table>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+`;
+
+   
+main.innerHTML += html;
+const recentTransactions = document.getElementById('recentTransactions');
+
     let html2;
     let i = 0;
-
+    
+    
   while(i < orders.length){
     const idOrder = orders[i].id;
     const firstNameOrder = orders[i].firstName;
     const lastNameOrder = orders[i].lastName;
     const payementMethodOrder = orders[i].payementMethod;
-
-    console.log(firstNameOrder);
+    const price = orders[i].totalPrice;
+  
 
      html2 = `
         <tr>
@@ -161,7 +179,7 @@ const AdminPage = async () => {
             </div>
           </td>
           <td class="border-bottom-0">
-            <h6 class="fw-semibold mb-0 fs-4">$3.9</h6>
+            <h6 class="fw-semibold mb-0 fs-4">$${price}</h6>
           </td>
       </tr>
         
@@ -206,6 +224,22 @@ const AdminPage = async () => {
 
 
   };
+
+  async function getTotalPriceOrder(orders){
+   
+    let sumPrice = 0;
+
+    orders.forEach((item) => {
+
+      if( item.totalPrice !== undefined){
+        sumPrice += item.totalPrice;
+      }
+
+    });
+   
+    return sumPrice;
+    
+  }
 
   
   export default AdminPage;
